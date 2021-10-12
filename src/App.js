97fe89825e97bc15937170746fcafb25c2b65321
OnLee3 <dlwotjr2@gmail.com>
2021-10-12 12:23:26 +0900

    Learning about State

--- src/App.js
+++ src/App.js
@@ -1,57 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-  id:1,
-  name: "rice",
-  image: "https://source.unsplash.com/random",
-  rating: 5
-},
-  {
-  id:2,
-  name: "pizza",
-  image: "https://source.unsplash.com/random",
-  rating: 4.3
-},
-  {
-  id:3,
-  name: "hamburger",
-  image: "https://source.unsplash.com/random",
-  rating: 4.5
-},
-  {
-  id:4,
-  name: "chicken",
-  image: "https://source.unsplash.com/random",
-  rating : 4.2
-},
-  {
-  id:5,
-  name: "tea",
-  image: "https://source.unsplash.com/random",
-  rating: 6
-},
-]
-
-function Food ({name, picture, rating}){
-  return (<div>
-    <h2>I don't like sweet {name}</h2>
-    <h4>{rating}/5</h4>
-    <img src={picture} alt={name}/>
-    </div>)
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
-
-function App() {
-  return <div>
-    {foodILike.map( dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>)}
-    </div>;
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies : []
+  }
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isLoading : false})}, 6000)
+  }
+  render(){
+    const { isLoading } = this.state;
+    return <div>
+      {isLoading ? "Loading" : "We are ready"}
+      </div>
+  }
 }
 
 export default App;
